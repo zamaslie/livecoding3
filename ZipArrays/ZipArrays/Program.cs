039a3217e1ee133175e902dcd435293cbf701ef5
zamaslie <54606830+zamaslie@users.noreply.github.com>
2019-12-03 19:10:49 -0500

    Add files via upload
    did livecoding

---
+++ ZipArrays/ZipArrays/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ZipArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array_one = { 1, 5, 2 };
+            int [] array_two = { 3, 6, 4 };
+
+            var zipped_array = new List<int>();
+            zipped_array.AddRange(array_one);
+            zipped_array.AddRange(array_two);
+
+            Console.WriteLine(zipped_array);
+            
+        }
+    }
+}
